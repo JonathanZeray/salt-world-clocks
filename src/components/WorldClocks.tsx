103d0db575e13b1fc0dfce6357666575825f0d3a
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-09 14:51:50 +0200

    Refactored

--- src/components/WorldClocks.tsx
+++ src/components/WorldClocks.tsx
@@ -2,11 +2,10 @@ import { useState, useEffect } from "react";
 import { Clock } from "./Clock";
 
 const getTimeForTimeZone = (timeZone: string): string => {
-  return new Intl.DateTimeFormat("en-US", {
+  return new Intl.DateTimeFormat("sv-SE", {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-    hourCycle: "h23",
     timeZone,
   }).format(new Date());
 };
